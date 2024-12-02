104602a946814ecf9943156790a5f56aeeaf4eac
Diego V <diegovitorgomesdias@gmail.com>
2024-12-02 00:43:43 -0300

    fix code

--- include/database.h
+++ include/database.h
@@ -11,13 +11,6 @@ int databaseGetAmountOfUsersRegisters();
 bool databaseHasUser(char* email);
 
 FILE* databaseGetUserFile();
-<<<<<<< HEAD
-ErrorController databaseInsertUser(User* user);
-ErrorController databaseRemoveUser(User* user);
-ErrorController databaseGetUser(char* email, User* user);
-ErrorController databaseUpdateAccount(Account* user, Money value);
-ErrorController databaseGetAccountByNumber(Number number, Account* account);
-=======
 FILE* databaseGetTransactionFile();
 
 ErrorController databaseInsertUser(User*);
@@ -28,9 +21,5 @@ ErrorController databaseGetUserByAccountNumber(Number account_number, User*);
 
 ErrorController databaseInsertTransaction(Transaction*);
 ErrorController databaseGetUserTransactions(User* user, LIST_Transaction* list);
->>>>>>> 3763a82296f5e6a65c977ce9b327c88a511b6056
-
-ErrorController databaseAddTransaction(Transaction* transaction);
-ErrorController databasePrintTransactions();
 
 #endif
